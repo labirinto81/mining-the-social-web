9c33c9440c26a9bf432edb100b5a983d9c4fa9d6
labirinto81 <34034496+labirinto81@users.noreply.github.com>
2019-10-10 17:47:26 +0200

    Changes for Hotfix 1

--- MyCode/Github/networkx.py
+++ MyCode/Github/networkx.py
@@ -1,12 +1,12 @@
 import networkx as nx
 
 #Creating the NULL graph
-G1 = nx.Graph()
+G2= nx.Graph()
 
 #Adding the edges will automatically add the nodes
-G1.add_edges_from([(1,2), (2,3), (3,4)])
+G2.add_edges_from([(1,2), (2,3), (3,4)])
 
 #Visulazing using matplotlib
 import matplotlib.pyplot as plt
-nx.draw(G1, with_labels=True)
+nx.draw(G2, with_labels=True)
 plt.show()
